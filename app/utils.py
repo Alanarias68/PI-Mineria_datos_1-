5571133e21dc68ae3dc9592446c1ffce53f33a4e
Alanarias68 <alanarias68@gmail.com>
2026-07-04 09:35:47 -0300

    Update data path for loading dataset

--- app/utils.py
+++ app/utils.py
@@ -5,5 +5,5 @@ import pandas as pd
 @st.cache_data
 def load_data():
     """Carga el dataset procesado de la plataforma de streaming."""
-    df = pd.read_json("dataset_processed.json")
+    df = pd.read_json("data/dataset_processed.json")
     return df
